8b166b0aed7aacd48225e62dc798079201f4ef05
Marcus Legendre <marcus.legendre@gmail.com>
2023-11-19 10:17:13 +0100

    feat: display info hash

--- src/torrent.rs
+++ src/torrent.rs
@@ -1,4 +1,5 @@
 use serde_bencode;
+use sha1::{Digest, Sha1};
 use std::fs;
 
 #[derive(serde::Serialize, serde::Deserialize, PartialEq, Eq, Debug)]
@@ -9,14 +10,28 @@ struct Torrent {
 
 #[derive(serde::Serialize, serde::Deserialize, PartialEq, Eq, Debug)]
 struct TorrentInfo {
-    length: i64,
+    name: String,
+    length: usize,
+    #[serde(rename = "piece length")]
+    piece_length: usize,
+    pieces: serde_bytes::ByteBuf,
 }
 
 pub fn info(file_name: &str) {
     let file_contents = fs::read(file_name).unwrap();
-
     let decoded_file_contents: Torrent = serde_bencode::from_bytes(&file_contents).unwrap();
+
+    let info_hash = calculate_info_hash(&decoded_file_contents.info);
 
     println!("Tracker URL: {}", decoded_file_contents.announce);
     println!("Length: {}", decoded_file_contents.info.length);
+    println!("Info Hash: {}", hex::encode(info_hash));
+}
+
+fn calculate_info_hash(torrent_info: &TorrentInfo) -> Vec<u8> {
+    let encoded_torrent_info = serde_bencode::to_bytes(torrent_info).unwrap();
+
+    let mut hasher = Sha1::new();
+    hasher.update(&encoded_torrent_info);
+    return hasher.finalize().to_ascii_lowercase();
 }
